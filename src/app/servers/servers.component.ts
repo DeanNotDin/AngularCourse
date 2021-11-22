8d8404f1bf981120e780e293ac1212952dff0657
DeanNotDin <dean@shemie.co>
2021-11-22 10:29:17 +0000

    Advancing through the course. Added button and events.

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,14 +3,28 @@ import {isPlatformServer} from "@angular/common";
 
 @Component({
   selector: 'app-servers',
-  template: '<app-server></app-server><app-server></app-server>',
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverStatus = "No servers created!"
+  serverName = "Server name"
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
   }
 
+  ngOnInit() {
+  }
+
+  onServerCreation() {
+    this.serverStatus = "Server was created!"
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
